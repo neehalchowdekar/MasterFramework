6672e5d8dd9e2a0dd3e84ec6693e015b63cbf5b8
Neehal <nehalchowdekar@gmail.com>
2022-03-06 23:07:40 +0530

    added URL method for selnium grid and selenoid

--- src/main/java/com/mfw/config/FrameworkConfig.java
+++ src/main/java/com/mfw/config/FrameworkConfig.java
@@ -1,9 +1,16 @@
 package com.mfw.config;
 
+import java.net.URL;
+
 import org.aeonbits.owner.Config;
 
 import com.mfw.config.converter.StringToBroswerTypeConverter;
+import com.mfw.config.converter.StringToRemoteModeBroswerTypeConverter;
+import com.mfw.config.converter.StringToRunModeBroswerTypeConverter;
+import com.mfw.config.converter.StringToURLTypeConverter;
+import com.mfw.enums.BrowserRemoteModeType;
 import com.mfw.enums.BrowserType;
+import com.mfw.enums.RunModeBrowserType;
 
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
@@ -16,5 +23,19 @@ public interface FrameworkConfig extends Config{
 	@DefaultValue("CHROME")
 	@ConverterClass(StringToBroswerTypeConverter.class)
 	BrowserType browser();
+	
+	@Key("runModeBrowser")
+	@ConverterClass(StringToRunModeBroswerTypeConverter.class)
+	RunModeBrowserType runModeForBrowser();
+	
+	@Key("browserRemoteMode")
+	@ConverterClass(StringToRemoteModeBroswerTypeConverter.class)
+	BrowserRemoteModeType browserRemoteMode();
+	
+	@ConverterClass(StringToURLTypeConverter.class)
+	URL seleniumGridURL();
+	
+	@ConverterClass(StringToURLTypeConverter.class)
+	URL selenoidGridURL();
 
 }
