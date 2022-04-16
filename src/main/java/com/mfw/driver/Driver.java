f111904a22d9890b56c087718225bb03517ad83f
Neehal <nehalchowdekar@gmail.com>
2022-04-16 14:26:10 +0530

    Modified the class with DriverManager getter and setter method for parallel execution

--- src/main/java/com/mfw/driver/Driver.java
+++ src/main/java/com/mfw/driver/Driver.java
@@ -3,39 +3,47 @@ package com.mfw.driver;
 import org.openqa.selenium.WebDriver;
 
 import com.mfw.config.factory.ConfigFactory;
-import com.mfw.driver.entity.DriverData;
+import com.mfw.driver.entity.MobileDriverData;
+import com.mfw.driver.entity.WebDriverData;
 import com.mfw.driver.factory.DriverFactory;
-import com.mfw.driver.factory.mobile.local.LocalMobileDriverFactory;
-import com.mfw.driver.factory.web.local.LocalDriverFactory;
-import com.mfw.driver.factory.web.remote.RemoteDriverFactory;
 import com.mfw.enums.MobilePlatformType;
 
 public final class Driver {
 	
 	private Driver() {}
+	
+	//public static WebDriver driver; 
+	// Problem : It is shared between thread, not thread safe 
+	// driver variable will be read and written
+	// The solution is to use ThreadLocal class
 
 	public static void initDriverForWeb() {
-		DriverData driverData = DriverData.builder()
-								.browserType(ConfigFactory.getConfig().browser())
-								.browserRemoteModeType(ConfigFactory.getConfig().browserRemoteMode())
-								.runModeType(ConfigFactory.getConfig().browserRunMode())
-								.build();
-		WebDriver driver = DriverFactory.getDriverForWeb(driverData);
-		driver.quit();
+		WebDriverData driverData = new WebDriverData(ConfigFactory.getConfig().browser(), ConfigFactory.getConfig().browserRemoteMode());
+//		WebDriverData driverData = WebDriverData.builder()
+//								.browserType(ConfigFactory.getConfig().browser())
+//								.browserRemoteModeType(ConfigFactory.getConfig().browserRemoteMode())
+//								.build();
+		WebDriver driver = DriverFactory
+				.getDriverForWeb(ConfigFactory.getConfig().browserRunMode())
+				.getDriver(driverData);
+		DriverManager.setDriver(driver);
 	}
 	
 	public static void initDriverForMobile() {
-		DriverData driverData = DriverData.builder()
-								.mobilePlatformType(MobilePlatformType.ANDROID)
-								.mobileRemoteModeType(ConfigFactory.getConfig().mobileRemoteMode())
-								.runModeType(ConfigFactory.getConfig().mobileRunMode())
-								.build();
-		WebDriver driver = DriverFactory.getDriverForMobile(driverData);
-		driver.quit();
+		MobileDriverData driverData = new MobileDriverData(MobilePlatformType.ANDROID, ConfigFactory.getConfig().mobileRemoteMode());
+//		MobileDriverData driverData = MobileDriverData.builder()
+//								.mobilePlatformType(MobilePlatformType.ANDROID)
+//								.mobileRemoteModeType(ConfigFactory.getConfig().mobileRemoteMode())
+//								.build();
+		WebDriver driver = DriverFactory
+				.getDriverForMobile(ConfigFactory.getConfig().mobileRunMode())
+				.getDriver(driverData);
+		DriverManager.setDriver(driver);
+		
 	}
 	
 	public static void quitDriver() {
-		
+		DriverManager.getDriver().quit();
 	}
 
 }
